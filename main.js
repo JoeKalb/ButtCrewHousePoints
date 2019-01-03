6440cc8f68ce1d226312c42b1fd9b232e1aa2e4b
Joe.Kalb <kalb.joseph@gmail.com>
2019-01-02 17:45:13 -0800

    added font and styled a bit better

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ let initialData = [{
   "points": 25
 }];
 
-let textColor = "black"
-let textOutline = "white"
+let textColor = "white"
+let textOutline = "black"
 
 // get points
 async function getCurrentPoints(){
@@ -43,17 +43,16 @@ function compareToChartData(updatedPoints){
 
 // display all info
 function renderPieChart(){
-  am4core.useTheme(am4themes_animated);
+  //am4core.useTheme(am4themes_animated);
   var chart = am4core.create("chartdiv", am4charts.PieChart);
   chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
   chart.data = initialData;
   chart.radius = am4core.percent(70);
-  chart.innerRadius = am4core.percent(40);
+  chart.innerRadius = am4core.percent(25);
   chart.startAngle = 180;
   chart.endAngle = 360;
    
-
   var series = chart.series.push(new am4charts.PieSeries());
   series.dataFields.value = "points";
   series.dataFields.category = "house";
@@ -65,11 +64,16 @@ function renderPieChart(){
     am4core.color("#00165e")
   ]
 
-  series.labels.template.text = "{category}: {value.value}"
-  series.labels.template.disabled = true;
-  series.ticks.template.disabled = true;
-  
+  series.labels.template.text = "{value.value}"
+  series.labels.template.disabled = false;
 
+  series.ticks.template.disabled = true;
+  series.alignLabels = false;
+  series.labels.template.radius = am4core.percent(-30)
+  series.labels.template.fill = textColor
+  series.labels.template.stroke = textOutline
+  series.labels.template.fontSize = 50
+  
   series.slices.template.cornerRadius = 10;
   series.slices.template.innerCornerRadius = 7;
   series.slices.template.draggable = true;
@@ -78,29 +82,17 @@ function renderPieChart(){
   series.hiddenState.properties.startAngle = 90;
   series.hiddenState.properties.endAngle = 90;
 
-  /*
-  chart.legend = new am4charts.Legend();
-  chart.legend.labels.template.text = ""
-  chart.legend.labels.template.fill = textColor
-  chart.legend.labels.disabled = true;
-  //chart.legend.labels.template.stroke = textOutline
-  chart.legend.valueLabels.template.text = "[bold]{name}: {value.value}"
-  chart.legend.valueLabels.template.fill = textColor
-  //chart.legend.valueLabels.template.stroke = textOutline
-  //chart.legend.itemContainers.template.paddingBottom = 250;
-  */
-
-
   // testing labels
   let label = series.createChild(am4core.Label);
   label.text = `| ${initialData[0].house}: ${initialData[0].points} | ${initialData[1].house}: ${initialData[1].points} | ${initialData[2].house}: ${initialData[2].points} | ${initialData[3].house}: ${initialData[3].points} |`;
-  label.fontSize = 20;
-  label.fontweight = "bold"
-  label.align = "";
+  label.fontSize = 50;
+  label.fontWeight = "bold"
+  label.align = "center";
   label.isMeasured = false;
-  label.x = -300;
+  label.x = -610;
   label.y = 30;
-  label.color = textColor
+  label.fill = textColor
+  label.stroke = textOutline
 }
 
 // get initial points when rendering
